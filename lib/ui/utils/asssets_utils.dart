fb1dc238d4102d3eb494c23f313391c414e48c8a
Ar G Ho <41030992+ArghoRoy92@users.noreply.github.com>
2023-08-18 23:42:27 +0600

    Delete lib directory

--- lib/ui/utils/asssets_utils.dart
+++
@@ -1,6 +0,0 @@
-class AssetsUtils{
-  AssetsUtils._();
-  static const String images='lib/assets/images';
-  static const String logoSVG='$images/logo.svg';
-  static const String backgroundSVG='$images/background.svg';
-}

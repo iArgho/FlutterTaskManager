f2cd155aecbf3c9e7a66e2c936d573e9e48b4469
Ar G Ho <41030992+ArghoRoy92@users.noreply.github.com>
2023-08-18 23:43:47 +0600

    Add files via upload

---
+++ lib/ui/screens/cancelled_task_screen.dart
@@ -0,0 +1,84 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:intro_widget/data/models/network_response.dart';
+import 'package:intro_widget/data/models/task_list_model.dart';
+import 'package:intro_widget/data/services/network_caller.dart';
+import 'package:intro_widget/data/utils/urls.dart';
+import 'package:intro_widget/ui/widgets/task_list_tile.dart';
+import 'package:intro_widget/ui/widgets/user_profile_banner.dart';
+
+class CancelledTaskScreen extends StatefulWidget {
+  const CancelledTaskScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CancelledTaskScreen> createState() => _CancelledTaskScreenState();
+}
+
+class _CancelledTaskScreenState extends State<CancelledTaskScreen> {
+  bool _getCancelledTaskScreen = false;
+  TaskListModel _taskListModel = TaskListModel();
+
+  Future<void> getCompletedTask() async {
+    _getCancelledTaskScreen = true;
+    if (mounted) {
+      setState(() {});
+    }
+    final NetworkResponse response =
+    await NetworkCaller().getRequest(Urls.inCancelledTask);
+    if (response.isSuccess) {
+      _taskListModel = TaskListModel.fromJson(response.body!);
+    } else {
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('In cancelled tasks get failed')));
+      }
+    }
+    _getCancelledTaskScreen = false;
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      getCompletedTask();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            const UserProfileAppBar(),
+            Expanded(
+              child: _getCancelledTaskScreen
+                  ? const Center(
+                child: CircularProgressIndicator(),
+              )
+                  : ListView.separated(
+                itemCount: _taskListModel.data?.length ?? 0,
+                itemBuilder: (context, index) {
+                  return TaskListTile(
+                    data: _taskListModel.data![index],
+                    onDeleteTap: () {},
+                    onEditTap: () {},
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) {
+                  return const Divider(
+                    height: 4,
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/ui/screens/completed_task_screen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:intro_widget/data/models/network_response.dart';
+import 'package:intro_widget/data/models/task_list_model.dart';
+import 'package:intro_widget/data/services/network_caller.dart';
+import 'package:intro_widget/data/utils/urls.dart';
+import 'package:intro_widget/ui/widgets/task_list_tile.dart';
+import 'package:intro_widget/ui/widgets/user_profile_banner.dart';
+
+class CompletedTaskScreen extends StatefulWidget {
+  const CompletedTaskScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CompletedTaskScreen> createState() => _CompletedTaskScreenState();
+}
+
+class _CompletedTaskScreenState extends State<CompletedTaskScreen> {
+  bool _getProgressTasksInProgress = false;
+  TaskListModel _taskListModel = TaskListModel();
+
+  Future<void> getCompletedTask() async {
+    _getProgressTasksInProgress = true;
+    if (mounted) {
+      setState(() {});
+    }
+    final NetworkResponse response =
+    await NetworkCaller().getRequest(Urls.inCompletedTasks);
+    if (response.isSuccess) {
+      _taskListModel = TaskListModel.fromJson(response.body!);
+    } else {
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('In completed tasks get failed')));
+      }
+    }
+    _getProgressTasksInProgress = false;
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      getCompletedTask();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            const UserProfileAppBar(),
+            Expanded(
+              child: _getProgressTasksInProgress
+                  ? const Center(
+                child: CircularProgressIndicator(),
+              )
+                  : ListView.separated(
+                itemCount: _taskListModel.data?.length ?? 0,
+                itemBuilder: (context, index) {
+                  return TaskListTile(
+                    data: _taskListModel.data![index],
+                    onDeleteTap: () {},
+                    onEditTap: () {},
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) {
+                  return const Divider(
+                    height: 4,
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
